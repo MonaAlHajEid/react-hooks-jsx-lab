727950bea5ad7fc54f0a4082c0ddbfb44b48d66a
Mona <monabadee@gmail.com>
2024-03-12 23:59:25 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,11 @@ import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  About
+  <h2>About Me</h2>
+  <p>I'm a Full-Stack web developer from jordan</p>
+  <img src={image} alt="I made this"/>
+  </div> );
 }
 
 export default About;
